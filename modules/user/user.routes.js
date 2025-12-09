8f5f3f457f2a0e2743c6260ae71640d40fc87fdf
md-zeon <zeon.cse@gmail.com>
2025-12-09 12:17:45 +0600

    refactor: update user routes for consistency and clarity

--- modules/user/user.routes.js
+++ modules/user/user.routes.js
@@ -8,14 +8,12 @@ const router = express.Router();
 router.post("/", userController.createUser);
 
 // get all users (admin only)
-//! /users => / => /user need to change in the frontend
 router.get("/", verifyFirebaseToken, verifyAdmin, userController.getAllUsers);
 
 // get user role
 router.get("/role", verifyFirebaseToken, userController.getUserRole);
 
 // get available Coins
-//! [NOTE: /available-coins => /user/available-coins] need to change frontend accordingly later
 router.get(
 	"/available-coins",
 	verifyFirebaseToken,
@@ -23,10 +21,8 @@ router.get(
 );
 
 // Update Micro Coins (increase/decrease)
-//! [NOTE: /update-coins/:email => /user/update-coins/:email] need to change frontend accordingly later
-//! [NOTE: email is required in the request params, and coinsToUpdate and status are required in the request body]
 router.patch(
-	"/update-coins/:email",
+	"/user/update-coins/:email",
 	verifyFirebaseToken,
 	userController.updateMicroCoins,
 );
@@ -40,7 +36,6 @@ router.delete(
 );
 
 // Update user role
-//! NOTE: /update-role/user/:id => /update-role/:id => /user/update-role/:id
 router.patch(
 	"/update-role/:id",
 	verifyFirebaseToken,
@@ -49,11 +44,9 @@ router.patch(
 );
 
 // Top 6 workers
-//! NOTE: /top-workers => /user/top-workers
 router.get("/top-workers", userController.getTopWorkers);
 
 // Update User Profile
-//! NOTE: /update-profile => /user/update-profile
 router.patch(
 	"/update-profile",
 	verifyFirebaseToken,
